9a94feb771cd1493c2214620af0463dfa78912a8
Andres Alba <bryanalba92@gmail.com>
2020-05-04 00:55:21 -0500

    Upload and get images

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,8 @@ var express = require('express');
 var UserController = require('../controllers/user');
 
 var md_auth = require('../middlewares/autenticated');
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir: './uploads/users'});
 
 var api = express.Router();
 
@@ -14,5 +16,7 @@ api.post('/login', UserController.loginUser);
 api.get('/user/:id', md_auth.ensureAuth, UserController.getUser);
 api.get('/users/:page?', md_auth.ensureAuth, UserController.getUsers);
 api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
+api.put('/upload-image-user/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
+api.get('/get-image-user/:imageFile', UserController.getImageFile);
 
 module.exports = api;
