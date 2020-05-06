dd07c3fee7e449c701764a5adac9ed7bdee5a9a4
Andres Alba <bryanalba92@gmail.com>
2020-05-06 01:19:11 -0500

    saveFollow, deleteFollow and getFollowinUsers

---
+++ routes/follow.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var express = require('express');
+var FollowController = require('../controllers/follow');
+var api = express.Router();
+var md_auth = require('../middlewares/autenticated');
+
+
+api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
+api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
+api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowinUsers);
+
+module.exports = api;
