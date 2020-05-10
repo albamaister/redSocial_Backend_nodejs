46553691d85ed3c2dea4d18bd14b3041ced484f0
Andres Alba <bryanalba92@gmail.com>
2020-05-10 01:24:51 -0500

    Upload Image and get image of publications

--- controllers/publication.js
+++ controllers/publication.js
@@ -90,10 +90,75 @@ function deletePublication(req, res) {
     })
 }
 
+
+function uploadImage(req, res){
+    var publicationId = req.params.id;
+ 
+    if(req.files){
+        var file_path = req.files.image.path;
+        console.log(file_path);
+        
+        var file_split = file_path.split('\\');
+        console.log(file_split);
+ 
+        var file_name = file_split[2];
+        console.log(file_name);
+ 
+        var ext_split = file_name.split('\.');
+        console.log(ext_split);
+ 
+        var file_ext = ext_split[1];
+        console.log(file_ext);
+ 
+        if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif'){
+
+            Publication.findOne({'user': req.user.sub, '_id': publicationId}).exec((err, publication) => {
+                if (publication) {
+                    // Actualizar documento de usuario logueado
+                    Publication.findByIdAndUpdate(publicationId, {file: file_name}, {new:true}, (err, publicationUpdated) =>{
+                       if(err) return res.status(500).send({message: 'Error en la petición'});
+        
+                       if(!publicationUpdated) return res.status(404).send({message: 'No se ha podido actualizar la publicacion'});
+        
+                       return res.status(200).send({publication: publicationUpdated});
+                    });
+                } else {
+                    return removeFilesOfUploads(res, file_path, 'No tienes permisos para actualizar');
+                }
+            });             
+        }else{
+            return removeFilesOfUploads(res, file_path, 'Extensión no válida');
+        }
+ 
+    }else{
+        return res.status(200).send({message: 'No se han subido imagenes'});
+    }
+}
+ 
+function removeFilesOfUploads(res, file_path, message){
+    fs.unlink(file_path, (err) => {
+        return res.status(200).send({message: message});
+    });
+}
+ 
+function getImageFile(req, res){
+    var image_file = req.params.imageFile;
+    var path_file = './uploads/publications/'+image_file;
+ 
+    fs.exists(path_file, (exists) => {
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        }else{
+            res.status(200).send({message: 'No existe la imagen...'});
+        }
+    });
+}
 module.exports = {
     probando,
     savePublication,
     getPublications,
     getPublication,
-    deletePublication
+    deletePublication,
+    uploadImage,
+    getImageFile
 }
